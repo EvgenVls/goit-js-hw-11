7c451634bca9fd7601f0fa2e46c5f2f0e3304392
EvgenVls <evgenvl@ukr.net>
2024-03-03 21:12:23 +0200

    all

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,43 +1,19 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import iconError from '../img/icon-error.png';
-
 export default searchImages;
 
-const searchParams = new URLSearchParams({
-    key: '42601887-7c2d316803b73c9107fd5176e',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-})
-
 function searchImages(tagImage) {
-    fetch(`https://pixabay.com/api/?${searchParams}&q=${tagImage}`)
+
+    const searchParams = new URLSearchParams({
+        key: '42601887-7c2d316803b73c9107fd5176e',
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+    })
+
+    return fetch(`https://pixabay.com/api/?${searchParams}&q=${tagImage}`)
     .then((response) => {
         if (!response.ok) {
             throw new Error(response.status);
         }
         return response.json();
     })
-        .then(data => {
-            const arrayImages = data.hits;
-            console.log(arrayImages.length);
-        if (arrayImages.length) {
-            console.log(arrayImages);
-            return arrayImages;
-            
-        } else {
-            iziToast.error({
-                iconUrl: iconError,
-                message: 'Sorry, there are no images matching your search query. Please try again!',
-                position: 'topRight',
-                backgroundColor: '#ef4040',
-                titleColor: '#FFFFFF',
-                messageColor: '#FFFFFF',
-                theme: 'dark',
-            });
-            }
-           
-    })
-    .catch((error) => console.log(error))
 }
--- src/main.js
+++ src/main.js
@@ -1,24 +1,48 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import iconError from './img/icon-error.png';
 import searchImages from './js/pixabay-api';
 import createMarcupGallery from './js/render-functions';
 
+
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
 
 form.addEventListener('submit', event => {
     event.preventDefault();
+    loader.style.display = 'grid';
     const tagImage = event.target.elements.search.value;
     if (tagImage) {
         form.reset();
-        console.log(tagImage);
-        
+        gallery.innerHTML = '';
         searchImages(tagImage)
-        .then((img) => console.log(img))
-        
-        // gallery.innerHTML = createMarcupGallery(images);// showGallery();// запуск функції побудови галереї
-
-        // const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250  });
+        .then(data => {
+        const arrayImages = data.hits;
+        loader.style.display = 'none';
+        if (arrayImages.length) {
+            gallery.innerHTML = createMarcupGallery(arrayImages);
+            const lightbox = new SimpleLightbox('.gallery a', {
+                captionsData: 'alt',
+                captionDelay: 250
+            });
+            lightbox.refresh();
+        } else {
+            iziToast.error({
+                iconUrl: iconError,
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                maxWidth: '432px',
+                position: 'topRight',
+                messageSize: 16,
+                backgroundColor: '#ef4040',
+                titleColor: '#FFFFFF',
+                messageColor: '#FFFFFF',
+                theme: 'dark',
+            });
+            }
+    })
+    .catch((error) => console.log(error))
     }
-    
 })
